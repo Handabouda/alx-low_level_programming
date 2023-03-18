e556d9074b9fa68cc6149fc7c1b9fd66e69ec679
Handabouda <belkhiria.hend@enis.tn>
2023-03-18 09:27:40 -0700

    yes

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,3 +1,16 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * prints a string
+ * Return: Always  0 (Success)
+ */
+
+int main(void)
+{
+	        puts("\"Programming is like building a multilingual puzzle");
+		        return (0);
+}
+
 int main(void)
 {
 	printf ("with proper grammar, but the outcome is a piece of art,");

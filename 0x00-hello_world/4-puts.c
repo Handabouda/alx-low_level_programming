eec5ec37fe24aaae2694fe3b4cfac5c5800ebff8
Handabouda <belkhiria.hend@enis.tn>
2023-03-18 09:14:37 -0700

    yes

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -8,11 +8,12 @@
  * main - Entry point
  *
  * prints a string
+ *
  * return: Always  0 (success)
  */
 
 int main(void)
-{	
+{
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }

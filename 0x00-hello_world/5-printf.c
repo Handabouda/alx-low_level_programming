45f0829aa7055cd42ebec81354423d734a25a551
Handabouda <belkhiria.hend@enis.tn>
2023-03-18 09:32:34 -0700

    yes

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -7,12 +7,6 @@
 
 int main(void)
 {
-	        puts("\"Programming is like building a multilingual puzzle");
-		        return (0);
-}
-
-int main(void)
-{
-	printf ("with proper grammar, but the outcome is a piece of art,");
+	printf("with proper grammar, but the outcome is a piece of art,");
 	return (0);
 }

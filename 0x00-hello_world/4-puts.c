0118af752a2232ec84491626df71edfac6e7fd15
Handabouda <belkhiria.hend@enis.tn>
2023-03-18 05:05:15 -0700

    yes

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,14 +1,16 @@
-/**
+/*
  * simple C program that uses the puts function
  * File: 4-puts.c
  */
 #include <stdio.h>
+
 /**
  * The main function of the program
  *
  * printing string using puts
- * return 0 (executed successfully)
+ * return: 0 (executed successfully)
  */
+
 int main(void)
 {	puts("\"Programming is like building a multilingual puzzle");
 	return (0);

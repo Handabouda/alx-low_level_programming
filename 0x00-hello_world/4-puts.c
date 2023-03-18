a19d7982de7d03df9a1e510a372d42b718ae6201
Handabouda <belkhiria.hend@enis.tn>
2023-03-18 04:47:39 -0700

    yes

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,8 @@
 #include <stdio.h>
-int main (void) 
-{	puts ("\"Programming is like building a multilingual puzzle");
+/**
+ * printing string using puts
+ */
+int main(void) 
+{	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }

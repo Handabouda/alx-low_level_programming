d6770af27469367186840df71490cb830063d805
Handabouda <belkhiria.hend@enis.tn>
2023-03-18 04:24:39 -0700

    yes

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,3 +1,6 @@
-#!/bin/bash
-echo '#include <stdio.h>' | cat - <(echo 'int main(void){puts("Programming is like building a multilingual puzzle");return (0);}') | gcc -x c -o puzzle - && ./puzzle && rm puzzle
+#include <stdio.h>
 
+int main() {
+	    puts("Programming is like building a multilingual puzzle");
+	        return 0;
+}

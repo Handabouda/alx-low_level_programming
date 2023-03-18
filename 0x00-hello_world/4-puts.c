7741f3055ce1361c25465a1c6eec38cd2ae9075b
Handabouda <belkhiria.hend@enis.tn>
2023-03-18 04:57:08 -0700

    yes

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,13 @@
+/**
+ * A simple C program that uses the `puts()` function to print a message to the console.
+ * @file 4-puts.c
+ */
 #include <stdio.h>
 /**
+ * The main function of the program.
  * main void
  * printing string using puts
- * return value 0
+ * return 0 if the program executed successfully.
  */
 int main(void)
 {	puts("\"Programming is like building a multilingual puzzle");

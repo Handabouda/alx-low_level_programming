033e29b18b9ec77ef852b713afc3ee325ef2f97c
Handabouda <belkhiria.hend@enis.tn>
2023-03-21 16:41:05 -0700

    yes

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -1,30 +1,15 @@
-#include"main.h"
+#include "main.h"
 
 /**
- * print_sign - print + if n is greater than zero,
- *             0 if n is zero and - if n is less
- *             than zero.
- *
- * @n: takes integer type input for function.
- *
- * Return: 1 if +, 0 if 0 and -1 if -
-*/
-
-int print_sign(int n)
+ * _abs - computes the absolute value of an integer
+ * @n: the int to check
+ * Return: the absolute value of int
+ */
+int _abs(int n)
 {
-	if (n > 0)
+	if (n >= 0)
 	{
-		_putchar(43 + 0);
-		return (1);
+		return (n);
 	}
-	else if (n == 0)
-	{
-		_putchar(0 + 48);
-		return (0);
-	}
-	else
-	{
-		_putchar(45 + 0);
-		return (-1);
-	}
+	return (-n);
 }

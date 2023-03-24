6f154f1e18039230a81f938048461113bfbd485b
Handabouda <belkhiria.hend@enis.tn>
2023-03-24 07:26:37 -0700

    yes

--- 0x03-debugging/3-print_remaining_days.c
+++ 0x03-debugging/3-print_remaining_days.c
@@ -12,14 +12,23 @@
 
 void print_remaining_days(int month, int day, int year)
 {
-	if (month < 1 || day > 31)
+	total_days = 265;
+	if ((year % 4 == 0 || year % 400 == 0) && !(year % 100 == 0))
+	{
+		total_days = 366;
+	}
+	if (month < 1 || day > 31 || day < 1 )
+	{
+		printf("Invalid date: %02d/%02d/%04d\n", month, day, year);
+	}
+	if (month == 2 && day == 29 && total_days == 366)
 	{
 		printf("Invalid date: %02d/%02d/%04d\n", month, day, year);
 	}
         else
         {
             printf("Day of the year: %d\n", day);
-            printf("Remaining days: %d\n", 365 - day);
+            printf("Remaining days: %d\n", total_days - day);
         }
     
 }

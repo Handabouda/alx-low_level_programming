24138c0a97d3c14532cf61d63ad41df3717d5d3b
Handabouda <belkhiria.hend@enis.tn>
2023-03-26 09:15:42 -0700

    yes

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main Entry point
+ *
+ * find out upper letter
+ * Return: 0 if min
+ * Return: 1 if maj
+ */
+
+int _isupper(int c)
+{
+	if (c >= 65 && c <= 90)
+	{
+		return (1);
+	}
+	else if (c >= 97 && c <= 122)
+	{
+		return (0);
+	}
+}

6007cd1c0b4fb605890a91ca17e155492b381249
Handabouda <belkhiria.hend@enis.tn>
2023-04-03 16:53:47 -0700

    rev

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -8,19 +8,19 @@
  */
 void rev_string(char *s)
 {
-    int len, i;
-    char temp;
+	int len, i;
+	char temp;
 
-    len = 0;
-    while (s[len] != '\0')
-    {
-        len++;
-    }
+	len = 0;
+	while (s[len] != '\0')
+	{
+		len++;
+	}
 
-    for (i = 0; i < len / 2; i++)
-    {
-        temp = s[i];
-        s[i] = s[len - i - 1];
-        s[len - i - 1] = temp;
-    }
+	for (i = 0; i < len / 2; i++)
+	{
+		temp = s[i];
+		s[i] = s[len - i - 1];
+		s[len - i - 1] = temp;
+	}
 }

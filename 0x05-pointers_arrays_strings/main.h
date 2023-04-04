4ed663649bbea3e53b902ae8fe3e2420c9db1257
Handabouda <belkhiria.hend@enis.tn>
2023-04-04 06:16:21 -0700

    print array

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -1,6 +1,7 @@
 #ifndef MAIN_H
 #define MAIN_H
 #include <stdio.h>
+#include <string.h>
 void reset_to_98(int *n);
 void swap_int(int *a, int *b);
 int _strlen(char *s);
@@ -8,4 +9,5 @@ void print_rev(char *s);
 void rev_string(char *s);
 void puts_half(char *str);
 char *_strcpy(char *dest, char *src);
+void print_array(int *a, int n);
 #endif

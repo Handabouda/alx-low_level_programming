6f83ffa0d46822064af619238be29ac9c451424f
Handabouda <belkhiria.hend@enis.tn>
2023-04-05 19:05:57 -0700

    puts

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * _puts_recursion- prints a string followed by a new line
+ * @s: pointer to string
+ * return: void
+ */
+void _puts_recursion(char *s)
+{
+	puts(s);
+	puts("\n");
+}

20e93d80f50c9f0fcc668a1cfc96c48239855626
Handabouda <belkhiria.hend@enis.tn>
2023-04-17 19:25:17 -0700

    structure

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,5 +1,18 @@
-struct dog {
-	char * name;
+#ifndef DOG
+#define DOG
+
+/**
+ * struct dog -dog class
+ * @name: dog name
+ * @age: dog age
+ * @owner: owner of the dog
+ */
+
+struct dog
+{
+	char *name;
 	float age;
-	char * owner;
-}
+	char *owner;
+};
+
+#endif

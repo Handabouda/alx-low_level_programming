9fb2c21e6945cc442886ebd5efcf81d91648a96a
Handabouda <belkhiria.hend@enis.tn>
2023-04-19 13:03:08 -0700

    I LOVE CODING IN C

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,33 @@
+#include "function_pointers.h"
+/**
+ * int_index - search for an integer
+ * @array: pointer to integer
+ * @size: size of array
+ * @cmp: pointer to int function
+ * Return: integer
+ */
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int i = 0;
+
+	if (size <= 0)
+	{
+		return (-1);
+	}
+	else if (array && cmp)
+	{
+		while (i < size)
+		{
+			if (cmp(array[i]) != 0)
+			{
+				return (cmp(array[i]));
+			}
+			else
+				i++;
+		}
+	}
+	else
+	{
+		return (-1);
+	}
+}

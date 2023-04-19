28914eb3d4ecaf65e1509cd18fa89a87839c8580
Handabouda <belkhiria.hend@enis.tn>
2023-04-19 09:24:54 -0700

    prints a name

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #ifndef function_pointers_H
 #define function_pointers_H
+#include <stddef.h>
 #include <string.h>
 int _putchar(char c);
 void print_name(char *name, void (*f)(char *));

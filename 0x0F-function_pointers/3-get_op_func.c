846749ce9e76307c8a67de0d1baff9763c51d4ad
Handabouda <belkhiria.hend@enis.tn>
2023-04-19 18:47:21 -0700

    I LOVE CODING IN C

---
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -0,0 +1,28 @@
+#include "3-calc.h"
+/*
+ * get_op_func - select the right function
+ * @s: pointer to operator
+ * Return: integer
+ */
+int (*get_op_func(char *s))(int, int)
+{
+	op_t ops[] = {
+		{"+", op_add},
+		{"-", op_sub},
+		{"*", op_mul},
+		{"/", op_div},
+		{"%", op_mod},
+		{NULL, NULL}
+	};
+	int i;
+
+	while (i < 6)
+	{
+		if (ops[i].op == s)
+		{
+			return (ops[i].f)
+		}
+		i++
+	}
+	return (NULL);
+}
---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,58 @@
+#include "3-calc.h"
+
+
+/*
+ * op_add - calculate the add of a and b
+ * @a: number 1
+ * @b: number 2
+ * Return: the add of a and b
+ */
+int op_add(int a, int b)
+{
+        return (a + b);
+}
+
+
+/*
+ * op_add - calculate the sum of a and b
+ * @a: number 1
+ * @b: number 2
+ * Return: the sum of a and b
+ */
+int op_sub(int a, int b)
+{
+        return (a - b);
+}
+
+/*
+ * op_mul - calculate the mul of a and b
+ * @a: number 1
+ * @b: number 2
+ * Return: the mul of a and b
+ */
+int op_mul(int a, int b)
+{
+        return (a * b);
+}
+
+/*
+ * op_div - calculate the div of a and b
+ * @a: number 1
+ * @b: number 2
+ * Return: the div of a and b
+ */
+int op_div(int a, int b)
+{
+        return (a / b);
+}
+
+/*
+ * op_mod - calculate the mod of a and b
+ * @a: number 1
+ * @b: number 2
+ * Return: the mod of a and b
+ */
+int op_mod(int a, int b)
+{
+        return (a % b);
+}

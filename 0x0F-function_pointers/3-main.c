8bfb24372d0defa298ef1fd3797feaf56797eb2d
Handabouda <belkhiria.hend@enis.tn>
2023-04-19 20:21:40 -0700

    I LOVE CODING IN C

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -8,7 +8,6 @@
  */
 int main(int argc, char *argv[])
 {
-	int num1, num2, result;
 	int (*func)(int, int);
 
 	if (argc != 4)
@@ -17,22 +16,18 @@ int main(int argc, char *argv[])
 		exit(98);
 	}
 
-	num1 = atoi(argv[1]);
-	num2 = atoi(argv[3]);
-
 	func = get_op_func(argv[2]);
-	if (func == NULL)
+	if (!func)
 	{
 		printf("Error\n");
 		exit(99);
 	}
-	if ((argv[2][0] == '/' || argv[2][0] == '%') && num2 == 0)
+	if ((argv[2][0] == '/' || argv[2][0] == '%') && atoi(argv[3]) == 0)
 	{
 		printf("Error\n");
 		exit(100);
 	}
-	result = func(num1, num2);
-	printf("%d\n", result);
+	printf("%d\n", oprt(atoi(argv[1]), atoi(argv[3])));
 
 	return (0);
 }

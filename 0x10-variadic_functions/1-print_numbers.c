460a4d8be0764b9d959bd87b808762c2756dd9cf
Handabouda <belkhiria.hend@enis.tn>
2023-04-25 20:28:30 -0700

    I LOVE CODING IN C

---
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -0,0 +1,22 @@
+#include "variadic_functions.h"
+/**
+ * print_numbers - prints numbers bollowed by a new line
+ * @separator: the string to be printed between functions
+ * @n: the number of integers passed to the function
+ * Return: void
+ */
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+	va_list args;
+
+	va_start(args, n);
+	if (!separator)
+	{
+		for (int i = 0; i < n; i++)
+		{
+			_putchar(va_arg(args, int);
+		}
+	}
+	va_end(args);
+}
+

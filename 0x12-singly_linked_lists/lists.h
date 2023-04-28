f23459442418a22d9c26ad290ced711886e02fa6
Handabouda <belkhiria.hend@enis.tn>
2023-04-27 17:48:22 -0700

    I love coding in C

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -1,5 +1,8 @@
 #ifndef _LINKED_LISTES
 #define _LINKED_LISTES
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
 /**
  * struct list_s - singly linked list
  * @str: string - (malloc'ed string)

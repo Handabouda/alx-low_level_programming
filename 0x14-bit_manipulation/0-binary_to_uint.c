892f80419bda42a67bf54f199b0b1f0fbc9d2307
Handabouda <belkhiria.hend@enis.tn>
2023-05-09 07:16:21 -0700

    love coding in C

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -52,6 +52,7 @@ int power(int b, int ex)
 unsigned int binary_to_uint(const char *b)
 {
 	int i;
+	int j = 0;
 	int sum = 0;
 
 	if (b == NULL)
@@ -62,9 +63,15 @@ unsigned int binary_to_uint(const char *b)
 		for (i = _strlen(b) - 1; i >= 0; i--)
 		{
 			if (b[i] == '1')
-				sum += 1 * power(2, i);
+			{
+				sum += 1 * power(2, j);
+				j++;
+			}
 			else if (b[i] == '0')
+			{
 				sum += 0;
+				j++;
+			}
 			else
 			{
 				return (0);

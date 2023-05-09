97eeaa295d0a269937bcf4d66adc467db02a9b1f
Handabouda <belkhiria.hend@enis.tn>
2023-05-09 06:26:07 -0700

    love coding in C

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,4 +1,30 @@
 #include "main.h"
+
+/**
+ * power - calculate the power
+ * @b: base
+ * @ex: exponent
+ * Return: the power of b by ex
+ */
+int power(int b, int ex)
+{
+        int i;
+        int r;
+
+        if (ex == 0)
+        {
+                return (1);
+        }
+        else
+        {
+                for (i = 0; i < ex; i++)
+                        r = r * b;
+        }
+        return (r);
+}
+
+
+
 /**
  * binary_to_uint - converts a binary number to an unsigned int
  * @b: pointing to a string of 0 and 1 chars

0637459293b9dcd8c9c44678d90e9d009167d3f2
Handabouda <belkhiria.hend@enis.tn>
2023-05-09 09:22:14 -0700

    love coding in C

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * print_binary - prints the binary representation of a number
+ * @n: the number to be converted
+ * Return: void
+ */
+void print_binary(unsigned long int n)
+{
+	if (n > 1)
+		print_binary(n >> 1);
+	putchar(n && 1);
+}
--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -6,6 +6,7 @@
 
 int _strlen(const char *str);
 int power(int b, int ex);
+void print_binary(unsigned long int n);
 unsigned int binary_to_uint(const char *b);
 
 #endif
